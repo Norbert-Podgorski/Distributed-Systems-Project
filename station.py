ce746076f0ef75a90702e858a8ed796324e35245
niziox <arekkontek@student.agh.edu.pl>
2023-04-03 00:22:32 +0200

    connect database to main

--- station.py
+++ station.py
@@ -17,11 +17,14 @@ class StationObservationArea:
 
 @ray.remote
 class Station(object):
-    def __init__(self, idx: int, observation_area: StationObservationArea):
+    def __init__(self, idx: int, observation_area: StationObservationArea, database):
         self.idx: int = idx
         self.observation_area: StationObservationArea = observation_area
         self.activity_detected: Dict[str, int] = {}
         self.create_report_structure()
+        self.database = database
+        self.database.add_station.remote(self.idx, self.observation_area.x_min, self.observation_area.x_max,
+                                         self.observation_area.y_min, self.observation_area.y_max)
 
     def simulate_new_activity(self) -> None:
         activity_x_coord = random.randint(self.observation_area.x_min, self.observation_area.x_max)
@@ -36,6 +39,10 @@ class Station(object):
         print("activity_x_coord: ", activity.getAttribute('x'))
         print("activity_y_coord: ", activity.getAttribute('y'))
         print("activity_power: ", activity.getAttribute('power'))
+        if len(activity.getAttribute('x')) > 0:
+            self.database.add_observation.remote(self.idx, int(activity.getAttribute('x')),
+                                                 int(activity.getAttribute('y')), int(activity.getAttribute('power')))
+
         self.clear_report()
 
     def create_report_structure(self) -> None:
