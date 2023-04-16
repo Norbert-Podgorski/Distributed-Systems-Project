835889683826b726c0d7a2b145170aa39256123d
niziox <arekkontek@student.agh.edu.pl>
2023-04-17 01:13:20 +0200

    add heatmap

---
+++ heatmap.py
@@ -0,0 +1,26 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+from sqlalchemy import create_engine
+
+
+def generate_heatmap():
+    db_string = "postgresql://postgres:postgres@localhost:5432/postgres"
+    engine = create_engine(db_string)
+
+    df_stations = pd.DataFrame(engine.execute('SELECT * FROM stations').fetchall())
+
+    for idx, station in enumerate(list(df_stations.station_idx)):
+        query = f'SELECT * FROM station_number_{station}'
+        if idx == 0:
+            df_observations = pd.DataFrame(engine.execute(query).fetchall())
+        else:
+            df_observations = pd.concat([df_observations, pd.DataFrame(engine.execute(query).fetchall())])
+
+    sns.scatterplot(data=df_observations, x='x', y='y', hue='station_idx', size='power')
+    plt.grid()
+    plt.show()
+
+
+if __name__ == '__main__':
+    generate_heatmap()
