300f176e47028e357a90525c1afd413b2580e36d
Norbert Podgórski <npodgorski@student.agh.edu.pl>
2023-05-21 14:01:04 +0200

    Heatmap grid and legend

--- heatmap.py
+++ heatmap.py
@@ -4,6 +4,12 @@ import seaborn as sns
 from sqlalchemy import create_engine
 
 
+def legend_without_duplicate_labels(figure):
+    handles, labels = plt.gca().get_legend_handles_labels()
+    by_label = dict(zip(labels, handles))
+    figure.legend(by_label.values(), by_label.keys(), bbox_to_anchor=(1.01, 1), loc='upper left')
+
+
 def generate_heatmap():
     db_string = "postgresql://postgres:postgres@localhost:5432/postgres"
     engine = create_engine(db_string)
@@ -18,8 +24,11 @@ def generate_heatmap():
             df_observations = pd.concat([df_observations, pd.DataFrame(engine.execute(query).fetchall())])
 
     sns.scatterplot(data=df_observations, x='x', y='y', hue='station_idx', size='power')
-    plt.savefig('./static/heatmap_pic.png')
+    legend_without_duplicate_labels(plt)
     plt.grid()
+    plt.tight_layout()
+    plt.savefig('./static/heatmap_pic.png')
+
 
 
 if __name__ == '__main__':
